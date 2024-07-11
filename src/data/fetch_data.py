d085c53703a45ac25bd663d68107f4b4bc5c8712
ronilpatil <ronilpatil2000@gmail.com>
2024-07-11 10:23:38 +0530

    bug fixes & enhancements in fetch_data and params.yaml

--- src/data/fetch_data.py
+++ src/data/fetch_data.py
@@ -1,20 +1,20 @@
 import pathlib
 import requests
-from retrying import retry # type: ignore
+import yaml
+from retrying import retry  # type: ignore
 from tqdm import tqdm
 
 
 @retry(
     stop_max_attempt_number=5, wait_fixed=2000
 )  # Retry 5 times with a 2-second wait between every attempt
-def download_file(url, local_filename):
+def download_file(url, local_filename, chunk_size):
     with requests.get(url, stream=True) as r:
         # raise exception if response is unsuccessfull
         r.raise_for_status()
         total_size = int(
             r.headers.get("content-length", 0)
         )  # Get the total size of the file
-        chunk_size = 8192
         with open(local_filename, "wb") as f:
             for chunk in tqdm(
                 r.iter_content(chunk_size=chunk_size),
@@ -29,16 +29,19 @@ def download_file(url, local_filename):
 if __name__ == "__main__":
 
     curr_dir = pathlib.Path(__file__)
-    home_dir = curr_dir.parent.parent.parent
+    home_dir = curr_dir.parent.parent.parent.as_posix()
+    params = yaml.safe_load(open(f"{home_dir}/params.yaml"))[
+        "fetch_data"
+    ]  # load params.yaml file to access parameters
 
-    year = "2019"
-    month = "011"
-    file_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month}.parquet"
+    file_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{params['year']}-{params['month']}.parquet"
 
     local_filename = file_url.split("/")[-1]
-    local_filepath = pathlib.Path(f"{home_dir.as_posix()}/{local_filename}")
+    local_filepath = pathlib.Path(
+        f"{home_dir}/data/external/{local_filename}"
+    )  # save data in ./data/external/ directory
 
     try:
-        download_file(file_url, local_filename)
+        download_file(file_url, local_filepath, chunk_size=params["chunk_size"])
     except requests.exceptions.RequestException as e:
         print(f"Failed to download the file after several attempts.\n{e}")
